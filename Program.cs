03c7db68aae13e9e70a4b04ac2465fd3ee491d67
Cab42 <Cab42@DESKTOP-VRK1TRC>
2022-11-15 09:33:52 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ну допустим код
+
+            for (int i = 0; i < 9; i++) 
+            {
+                Console.WriteLine("aboba");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
